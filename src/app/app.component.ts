7cd67030449d6cfca78e392d4404f515e4b01d24
imomark <khanomarkhalid@gmail.com>
2020-08-15 19:43:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+
+  pages = [];
+  show = false;
+  constructor(private wiki: WikipediaService){}
+  onTerm(term: string){
+    this.wiki.search(term).subscribe((response: any)=>{
+      this.pages = response.query.search;
+      console.log(response.query.search);
+
+    });
+
+  }
 }
